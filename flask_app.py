6d0275b39c5d9ef1bac0e8967c4151779c88923c
Ridog <4443035+Ridog@users.noreply.github.com>
2023-03-19 14:47:32 +0100

    added confirmation route and moved some lines for better readability

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_socketio import SocketIO, send
 from image_processing import process_images, clear_images_directory
+from Salai import PassPromptToSelfBot, Upscale
 import Globals
 
 app = Flask(__name__)
@@ -11,10 +12,34 @@ socketio = SocketIO(app, async_mode='threading')
 def prompt():
     return render_template("prompt.html")
 
+@app.route("/confirmation", methods=["GET"])
+def confirmation():
+    return render_template("confirmation.html")
+
 # Route for rendering the final landing page
 @app.route("/final_landing", methods=['GET'])
 def final_landing():
     return render_template("final_landing.html")
+
+# Route for processing the user input and generating images
+@app.route("/", methods=["POST"])
+def process_prompt():
+    clear_images_directory("images")
+    user_input = request.form["prompt"]
+    user_input += Globals.PROMPT_SUFFIX
+
+    num_calls = 2 if Globals.MODE == "Red Room" else 1
+
+    for _ in range(num_calls):
+        response = PassPromptToSelfBot(user_input)
+        if response.status_code >= 400:
+            print(response.text)
+            print(response.status_code)
+        else:
+            print("Your image is being prepared, please wait a moment...")
+
+    # Redirect to the confirmation page
+    return redirect(url_for('confirmation'))
 
 # WebSocket endpoint for notifying the client when the expected number of images are ready
 @socketio.on('connect')
@@ -38,25 +63,7 @@ def websocket_endpoint():
 
     socketio.start_background_task(target=check_images)
 
-# Route for processing the user input and generating images
-@app.route("/", methods=["POST"])
-def process_prompt():
-    clear_images_directory("images")
-    user_input = request.form["prompt"]
-    user_input += Globals.PROMPT_SUFFIX
 
-    num_calls = 2 if Globals.MODE == "Red Room" else 1
-
-    for _ in range(num_calls):
-        response = PassPromptToSelfBot(user_input)
-        if response.status_code >= 400:
-            print(response.text)
-            print(response.status_code)
-        else:
-            print("Your image is being prepared, please wait a moment...")
-
-    # Redirect to the confirmation page
-    return redirect(url_for('confirmation'))
 
 def start_flask_app():
     app.run(host='0.0.0.0', port=8000)
